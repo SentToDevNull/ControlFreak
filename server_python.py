4dc663123867ae5ec0d3f386441ae0bdaf4d1f94
Prakol <praneeth.kolichala@gmail.com>
2016-12-01 17:20:54 -0500

    commented code

--- server_python.py
+++ server_python.py
@@ -9,17 +9,27 @@ from autobahn.twisted.websocket import WebSocketServerFactory, \
 from autobahn.twisted.resource import WebSocketResource
 from turn_lights import Light
 
+# This will be the files latest_coords
+# TODO: we should put all of these (latest_coords, button_pushed etc.)
+# in a single class called UI (or something like that)
 latest_coords = [0, 0]
+# This is a websocket server protocol
+# It tells us what to do when we receive a websocket request
 class SomeServerProtocol(WebSocketServerProtocol):
     def onConnect(self, request):
+        # Every time a websocket handshake takes place
+        # (Currently, just logs the details of request)
         print("Connection {}".format(request))
     def onMessage(self, payload, isBinary):
+        # Currently the payload comes in the format:
+        # "coords: 0.6 0.4"
         string = payload.decode('utf8')
         newPower = float(string.split(" ")[1])*100
         print("Power", newPower)
         latest_coords[0] = newPower
     def onOpen(self):
         print("something opened")
+
 class MainServer:
   def __init__(self):
     self.latest_coords = latest_coords
@@ -41,5 +51,7 @@ class MainServer:
     l = Light()
     LoopingCall(l.loop, latest_coords).start(0)
     reactor.run()
-m = MainServer()
-m.runReactor()
+
+if __name__ == "__main__":
+    m = MainServer()
+    m.runReactor()
--- turn_lights.py
+++ turn_lights.py
@@ -7,35 +7,16 @@ GREEN_LED = 18
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# Set the GPIO pins we're going to use to for output.  This is
-# necessary if using simple output to pull pins high or low, and
-# also when using PWM.
 GPIO.setup(GREEN_LED, GPIO.OUT)
 
-# Note this is a software based implementation of PWM, which is not 
-# as accurate as hardware PWM.
-# This should be a frequency that works with the Talon motor controller
-# pwm1 = GPIO.PWM(GREEN_LED, 50)
-
-# Start each PWM object with a duty cycle of 0 (off)
-# Duty cycle is the % of the complete cycle that the pin is pulled high.
-# i.e, the % of time the device is on.
-# Range of duty cycle is 0.0 <= dc <= 100.0
-
 class Light:
-    def __init__(self):
-      # self.CURRENT_POWER = 50
-      self.running = False
-    def run(self):
-        self.running = True
-
-        # Cleanup
-        # pwm1.stop()
-        GPIO.cleanup()
+    # This will be called over and over
+    # It must be in this method
+    # so that the server can update the server/respond to clients
+    # The only parameter is latest_coords
+    # which provides the coordinates of the joystick between 0 and 100
     def loop(self, latest_coords):
         if latest_coords[0] > 80:
             GPIO.output(GREEN_LED, True)
         else:
             GPIO.output(GREEN_LED, False)
-    def stop(self):
-        self.running = False
